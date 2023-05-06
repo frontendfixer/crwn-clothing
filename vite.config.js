407d155d76a7ec32eeb5c89d1ffa252fa0567663
Lakshmikanta <frontendfixer@github.com>
2023-05-06 22:35:50 +0530

    remove image optimizer and add new favicons

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteImageOptimizer(), svgr()],
+  plugins: [react(), svgr()],
   server: {
     port: 5555,
     strictPort: true,
